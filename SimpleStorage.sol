ca070e823bcfb99bd5f83c5b2157300c5ed78140
HectorSM <hsolarem@gmail.com>
2026-02-11 00:28:23 -0800

    Variables and function example

---
+++ SimpleStorage.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract SimpleStorage {
+
+    //Datatypes   
+    bool hasFavoriteNumber = true;
+    uint256 favoriteNumber = 88; //Unsigned - only positive values, default 0
+    int256 favNumber = -5;
+    string favNumberString = "twenty";
+    address favAddres = 0xC2076A08bCe8B5FD9128Df8F25F4307df2f2F29f;
+    bytes32 favoritebytes = "32";
+
+
+    //Function
+    uint256 public myNumber;
+
+    function store(uint256 _myNumber) public {
+        myNumber = _myNumber;
+    }
+
+    //view - only reads the state (reads a store variable)     
+    function getMyNumber() public view returns (uint256) {
+        return myNumber;
+    }
+
+    //pure - Do not read state
+    function getANumber() public pure returns (uint256) {
+        return 8;
+    }
+
+
+}
