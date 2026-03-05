519e31875c0697aeef8c26941fa51fd745eab458
HectorSM <hsolarem@gmail.com>
2026-03-04 23:52:17 -0800

    Library impl

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,22 +2,22 @@
 pragma solidity ^0.8.19;
 
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 
 contract FundMe {
 
+
+    using PriceConverter for uint256;
 
     uint256 minUsd = 5e18;
     address[] funders;
     mapping (address funder => uint256 amount) public funderToAmount;
 
 
-    function fund () public  payable {
-
-        
-        uint256 ethInUsd = convertEthToUsd(msg.value);
-        require(ethInUsd >= minUsd, "Did not send enought ETH");
+    function fund () public  payable {     
+     
+        require(msg.value.convertEthToUsd() >= minUsd, "Did not send enought ETH");
 
         //Register funders
         funders.push(msg.sender);
@@ -28,38 +28,6 @@ contract FundMe {
 
     function withdraw () public {
     }
-
-
-    /*Function to convert ETH to USD*/
-    function convertEthToUsd(uint256 _ethValue) internal view returns (uint256) {
-
-        /*
-            Example
-            1 ETH -> 2000_0000000000_00000000
-
-            (2000_0000000000_00000000 * 1_0000000000_00000000) / 1e18 
-            2000_0000000000_00000000 = 1 ETH
-
-        */
-
-        AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-
-        (
-        /* uint80 roundId */
-        ,
-        int256 answer,
-        /*uint256 startedAt*/
-        ,
-        /*uint256 updatedAt*/
-        ,
-        /*uint80 answeredInRound*/
-        ) = dataFeed.latestRoundData();
-          
-        // ETH/USD rate in 18 digit  - ETH price in term of USD
-        uint256 ethPrice =  uint256(answer * 1e10);//2000_0000000000_00000000
-      
-        return (ethPrice * _ethValue) / 1e18;//(2000_0000000000_00000000 * 1_0000000000_00000000) / 1e18   = 2000_0000000000_00000000
-
-    }
+    
 
 }
---
+++ contracts/PriceConverter.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+
+
+library PriceConverter {
+
+    /*Function to convert ETH to USD*/
+    function convertEthToUsd(uint256 _ethValue) public view returns (uint256) {
+
+        /*
+            Example
+            1 ETH -> 2000_0000000000_00000000
+
+            (2000_0000000000_00000000 * 1_0000000000_00000000) / 1e18 
+            2000_0000000000_00000000 = 1 ETH
+
+        */
+
+        AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+
+        (
+        /* uint80 roundId */
+        ,
+        int256 answer,
+        /*uint256 startedAt*/
+        ,
+        /*uint256 updatedAt*/
+        ,
+        /*uint80 answeredInRound*/
+        ) = dataFeed.latestRoundData();
+          
+        // ETH/USD rate in 18 digit  - ETH price in term of USD
+        uint256 ethPrice =  uint256(answer * 1e10);//2000_0000000000_00000000
+      
+        return (ethPrice * _ethValue) / 1e18;//(2000_0000000000_00000000 * 1_0000000000_00000000) / 1e18   = 2000_0000000000_00000000
+
+    }
+
+}
